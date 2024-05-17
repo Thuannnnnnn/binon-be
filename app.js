4d312d984ceb725d54f8d032a7e92a39c875c963
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-17 23:38:26 +0700

    a

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql');
+require('dotenv').config();
 var cors = require('cors')
+
+let connection = mysql.createConnection({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) return console.error(err.message);
+
+  console.log('Connected to the MySQL server.');
+});
 app.use(cors('shopcuathuan.shop'))
 app.get('/api/hello', (req, res) => {
-  res.send('data đã deploy')
+  let connection = mysql.createConnection({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  connection.connect((err) => {
+    if (err) return res.send('err');
+
+    res.send('Connected to the MySQL server.');
+  });
+
 })
 
 app.listen(port, () => {

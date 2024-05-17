6fe0ccb207302f46eb4d18b84b8cd4e8f8caa6ad
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-17 23:28:32 +0700

    avb

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var cors = require('cors')
+app.use(cors('shopcuathuan.shop'))
 app.get('/api/hello', (req, res) => {
   res.send('data đã deploy')
 })

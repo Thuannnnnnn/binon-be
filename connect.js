755b3f3efc1236dfb064dcadf05263f8e6718010
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-18 00:11:30 +0700

    a

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 let connection = mysql.createConnection({
@@ -11,7 +11,28 @@ let connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-    if (err) return console.error(err.message);
-
+    if (err) {
+        return console.error('Error connecting to the MySQL server: ', err.message);
+    }
     console.log('Connected to the MySQL server.');
+
+    // Truy vấn để lấy dữ liệu từ bảng users
+    const query = 'SELECT * FROM users';
+
+    connection.query(query, (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query: ', err.message);
+            return;
+        }
+        console.log('Data from users table:');
+        console.log(results);
+
+        // Đóng kết nối sau khi hoàn thành truy vấn
+        connection.end((err) => {
+            if (err) {
+                return console.log('Error ending the connection: ', err.message);
+            }
+            console.log('Connection closed.');
+        });
+    });
 });

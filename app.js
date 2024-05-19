cc45ad77ff4fb8532fcfbf1d70ffcf134c7c7fc3
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-19 16:14:56 +0700

    a

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(cors('shopcuathuan.shop'))
 app.use(express.json())
 const product = require('./routes/product')
 app.use('/api/product', product)
+app.get('/api/hello', (req, res) => {
+
+  res.send('Connected to the MySQL server.');
+});
+
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })

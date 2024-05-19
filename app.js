ce1e8d924ff5bc499d1fe18a7c52babde4f732ad
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-19 16:26:02 +0700

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 const mysql = require('mysql2');
 require('dotenv').config();
 var cors = require('cors')

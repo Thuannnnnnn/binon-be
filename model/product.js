aff1e3514842166a21af6719da5c1525bf331ac8
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-21 21:31:03 +0700

    v

--- model/product.js
+++ model/product.js
@@ -2,7 +2,6 @@ const { query } = require('../connect.js')
 const productModel = {
     async getAllProduct() {
         try {
-            console.log("a")
             const [result] = await query('SELECT * FROM product')
             return result
         } catch (err) {

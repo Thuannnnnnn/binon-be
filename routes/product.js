046d313caa0309eb3f5cf61ff20dfd415eea6780
Thuannnnnnn <133732703+Thuannnnnnn@users.noreply.github.com>
2024-05-24 15:15:29 +0700

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { verify } = require('../middleware/authToken')
-const productController = require('../controller/product');
+const { verify } = require("../middleware/authToken");
+const productController = require("../controller/product");
 
-router.get('/getAll', verify(), productController.getAllProduct);
-router.post('/add', verify(), productController.addProduct);
-
+router.get("/getAll", verify(), productController.getAllProduct);
+router.post("/add", verify(), productController.addProduct);
+router.put("/edit/:productID", verify(), productController.editProduct);
+router.delete("/delete/:productID", verify(), productController.deleteProduct);
 
 module.exports = router;
